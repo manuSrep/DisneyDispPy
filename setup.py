43917b3adc680f243b42a9970caecd1d44d415c6
Manuel Tuschen <Manuel_Tuschen@web.de>
2016-07-04 15:30:45 +0200

    Restore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='DisneyDisp',
+      version='0.1',
+      description='A python implementation of the \"Scene Reconstruction from High Spatio-Angular Resolution Light Fields\". For more information see https://www.disneyresearch.com/project/lightfields/.',
+      url='https://github.com/manuSrep/DisneyDispPy.git',
+      author='Marcel Gutsche, Manuel Tuschen',
+      author_email='Manuel_Tuschen@web.de',
+      license='GPL3 License',
+      packages=['DisneyDisp'],
+      install_requires=["scipy","numpy","scikit-image","matplotlib","numba","h5py","progressbar2","easyScripting"],
+      zip_safe=False)
